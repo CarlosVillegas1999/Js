c32ad6fdbdeba8d0ef066a044319aeca4e8e742a
CarlosVillegas1999 <villegascarlos530@gmail.com>
2024-06-24 17:13:20 -0500

    Arrays

--- arreglosObjetos.js
+++ arreglosObjetos.js
@@ -1,1 +1,18 @@
-console.log('Hola Mundo');
+// Arreglos en JavaScript
+
+
+var nombre = ['Carlos', 'Adriana', 'Samuel', 'Cristian'];
+
+var vegetales = new Array('Tomates', 'Zanahoria', 'Lechuga');
+
+console.log(nombre[2]);
+console.log(vegetales[1]);
+
+nombre[2] = 'Jose';
+console.log(nombre[2]);
+
+vegetales[1] = 'Nabo';
+console.log(vegetales[1]);
+
+console.log(nombre.length);
+console.log(vegetales.length);
