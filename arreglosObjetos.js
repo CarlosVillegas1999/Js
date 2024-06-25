1786c56903158829e180f505c46a66c6a3270ead
CarlosVillegas1999 <villegascarlos530@gmail.com>
2024-06-25 00:16:11 -0500

    Metodos para arrays

--- arreglosObjetos.js
+++ arreglosObjetos.js
@@ -1,6 +1,6 @@
 // Arreglos en JavaScript
 
-
+/*
 var nombre = ['Carlos', 'Adriana', 'Samuel', 'Cristian'];
 
 var vegetales = new Array('Tomates', 'Zanahoria', 'Lechuga');
@@ -16,3 +16,37 @@ console.log(vegetales[1]);
 
 console.log(nombre.length);
 console.log(vegetales.length);
+*/
+
+
+/***
+ * Operaciones con arreglos
+ */
+
+var frutas = ['Pera', 'Manzanas','Uvas','Sandia'];
+console.log(frutas);
+
+/*for(i=0; i<=frutas.length - 1;i++){
+    console.log(frutas[i]);
+}*/
+
+/*
+frutas.forEach(function (elemento, indice, array){
+    console.log(elemento, indice);
+});
+*/
+
+frutas.push('Naranja');  //Agregar elemento
+console.log(frutas);
+frutas.unshift('fresa'); //Agregar elemento al inicio
+console.log(frutas);
+frutas.pop();  //Eleminar elemento al final
+console.log(frutas);
+frutas.shift();  //Eliminar elemento al inicio
+console.log(frutas);
+
+var pos = frutas.indexOf('Uvas');  //Que me muestre la posicion del elemento
+console.log(pos);
+
+frutas.splice(1,2); //eliminar elemento desde una posicion y la cantidad de elementos a eliminar 
+console.log(frutas);
