62fb3831b769483e2b97ed9e04913ba18fafd601
CarlosVillegas1999 <villegascarlos530@gmail.com>
2024-06-25 01:16:52 -0500

    fin arreglos y objetos

--- arreglosObjetos.js
+++ arreglosObjetos.js
@@ -23,7 +23,7 @@ console.log(vegetales.length);
  * Operaciones con arreglos
  */
 
-var frutas = ['Pera', 'Manzanas','Uvas','Sandia'];
+/*var frutas = ['Pera', 'Manzanas','Uvas','Sandia'];
 console.log(frutas);
 
 /*for(i=0; i<=frutas.length - 1;i++){
@@ -36,7 +36,7 @@ frutas.forEach(function (elemento, indice, array){
 });
 */
 
-frutas.push('Naranja');  //Agregar elemento
+/*frutas.push('Naranja');  //Agregar elemento
 console.log(frutas);
 frutas.unshift('fresa'); //Agregar elemento al inicio
 console.log(frutas);
@@ -50,3 +50,64 @@ console.log(pos);
 
 frutas.splice(1,2); //eliminar elemento desde una posicion y la cantidad de elementos a eliminar 
 console.log(frutas);
+*/
+
+
+//Arreglo con elementos de diferentes tipos
+/*
+var persona = ['Carlos', 'Villegas', 23,'123231332',1.65];
+
+console.log(persona);
+*/
+
+
+//Objetos Literales
+
+/*
+var persona = {
+   nombre: 'Carlos',
+   apellido: 'Villegas',
+   gustos: ['Warframe','Anime','Minecraft','Musica'],
+   Trabajo: 'Programador',
+   esCasado: false
+};
+
+console.log(persona.apellido);
+console.log(persona['Trabajo']);
+
+
+/***************
+ * Objetos con la sintaxis Object
+ */
+
+/*
+var persona2 = new Object();
+persona2.nombre = 'Andres';
+persona2.apellido = 'Rendon';
+persona2['Trabajo'] = 'conductor';
+
+console.log(persona2);
+*/
+
+/*****
+ * Objetos y Metodos
+ */
+
+
+var persona = {
+    //Propiedades
+    nombre: 'Carlos',
+    apellido: 'Villegas',
+    gustos: ['Warframe','Anime','Minecraft','Musica'],
+    Trabajo: 'Programador',
+    esCasado: false,
+    yearNacimiento: 1985,
+
+    //Metodos
+    calcularEdad: function(){
+        this.edad = 2019 - this.yearNacimiento;
+    }
+ };
+
+ persona.calcularEdad();
+ console.log(persona);
