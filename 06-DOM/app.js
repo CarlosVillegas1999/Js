9aec72d318f148dfc3184a98548644a1115b3c0c
CarlosVillegas1999 <villegascarlos530@gmail.com>
2024-08-14 20:49:37 -0500

    Agregar un elemento a la lista

--- 06-DOM/app.js
+++ 06-DOM/app.js
@@ -165,3 +165,33 @@ function hacerClic(){
     console.log('Usted hizo clic');
 }
 */
+
+
+///Agregar un elemento a la lista 
+
+
+var form = document.getElementById("formAgregar");
+var lista = document.getElementById("items");
+
+form.addEventListener('submit', agregarItem);
+function agregarItem(e) {
+    e.preventDefault();
+    var newItem = document.getElementById('item').value;
+
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+
+    var botonDel = document.createElement('button');
+    botonDel.className = 'btn btn-danger btn-sm float-right eliminar'
+    botonDel.appendChild(document.createTextNode('X'));
+
+    li.appendChild(botonDel);
+    console.log(li);
+
+    lista.appendChild(li);
+}
+
+
+
+
