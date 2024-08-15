484bc5bb6fdc40f55ffda20063a99b59782c98a1
CarlosVillegas1999 <villegascarlos530@gmail.com>
2024-08-15 18:26:48 -0500

    Fin de la clase de manipulaciòn del DOM

--- 06-DOM/app.js
+++ 06-DOM/app.js
@@ -172,8 +172,19 @@ function hacerClic(){
 
 var form = document.getElementById("formAgregar");
 var lista = document.getElementById("items");
+var filtro = document.getElementById("filtro");
 
+//Evento submit del formulario 
 form.addEventListener('submit', agregarItem);
+
+//Evento clic de la lista
+lista.addEventListener('click', eliminarItem)
+
+//Evento del teclado en el campo de filtro 
+filtro.addEventListener('keyup', filtrarItems)
+
+
+//Funcion para agregar un item a la lista 
 function agregarItem(e) {
     e.preventDefault();
     var newItem = document.getElementById('item').value;
@@ -183,14 +194,48 @@ function agregarItem(e) {
     li.appendChild(document.createTextNode(newItem));
 
     var botonDel = document.createElement('button');
-    botonDel.className = 'btn btn-danger btn-sm float-right eliminar'
+    botonDel.className = 'btn btn-danger btn-sm float-right eliminar';
     botonDel.appendChild(document.createTextNode('X'));
 
     li.appendChild(botonDel);
-    console.log(li);
 
     lista.appendChild(li);
 }
+
+
+//Funcion para eliminar un item de la lista 
+
+function eliminarItem(e) {
+    if (e.target.classList.contains('eliminar')) {
+        if (confirm('¿Seguro que quieres eliminar el elemento?')) {
+            var li = e.target.parentElement;
+            lista.removeChild(li);
+        }
+    }
+}
+
+
+//Funcion para filtrar elementos de la lista
+
+function filtrarItems(e) {
+    var texto = e.target.value.toLowerCase();
+    var items = lista.getElementsByTagName('li');
+    Array.from(items).forEach(function (item) {
+        var itemNombre = item.firstChild.textContent;
+        if (itemNombre.toLowerCase().indexOf(texto) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+
+    console.log(items)
+}
+
+
+
+
+
 
 
 
