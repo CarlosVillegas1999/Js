bd335919040b0fd9555eb9eb99dda3819dedc03f
CarlosVillegas1999 <villegascarlos530@gmail.com>
2024-08-16 15:00:39 -0500

    JavaScriptModerno

---
+++ javascriptModerno/app.js
@@ -0,0 +1,37 @@
+// ES5 - Variables
+/*
+var nombre5 = 'Carlos';
+var edad = 25;
+nombre5 = 'Andres';
+
+// ES6 - Variables
+
+const nombre6 = 'Samu';
+let edad6 = 28;
+*/
+
+//Template Strings
+/*
+let nombre = 'Carlos';
+let apellido = 'villegas';
+const nacimiento = 1999;
+const cuidad = 'medellin';
+
+function calcularEdad(year) {
+    return 2024 - year;
+}
+
+
+//ES5
+
+console.log(nombre + ' ' + apellido + ' nació en ' + cuidad + ' su edad es ' + calcularEdad(nacimiento));
+
+//ES6
+
+console.log(`${nombre} ${apellido}, nació en ${cuidad}, y su edad es ${calcularEdad(nacimiento)}`);
+
+
+
+let nombreCompleto = `${nombre} ${apellido}`;
+console.log(nombreCompleto);
+*/
